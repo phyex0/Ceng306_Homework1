7667192802ea654f06da3ac45dee027146572edc
phyex0 <phyex0-halit_burak@outlook.com>
2021-05-02 04:17:28 +0300

    .

--- src/CarTest.java
+++ src/CarTest.java
@@ -7,10 +7,27 @@ public class CarTest {
 
     @Test
     void constructorTest() throws Exception {
+
+        Lock l = new Lock();
+
+        Key k = new Key(new Lock());
+        Key k1 = new Key(l);
+
+        Door d= new Door(k.getLock());
+        Door d1= new Door(l);
+        Door d2= new Door(l);
+
+        Door[] dArr ={d,d1,d2};
+
+        dArr[0].unlock(k);
+        dArr[1].unlock(k1);
+        dArr[2].unlock(k1);
+
         assertEquals(null,new Car(null,null).getControlPanel());
         ControlPanel cp= new ControlPanel(50);
         //Line 12 failed. To solve it throw Exception is added to methods of Control panel class. if your control panel is null you cannot use any methods of it.
         Assertions.assertThrows(Exception.class ,()->{new Car(null,cp).getControlPanel().isRunning();});
+        assertNotEquals(null,new Car(dArr,cp));
     }
 
     @Test
